936e56e003e2ec30d5c8289b8764aaa5e4b4d41e
Findsay <fiona.e.lindsay@gmail.com>
2018-01-09 18:34:38 +0000

    Xhr added

--- src/containers/SongContainer.jsx
+++ src/containers/SongContainer.jsx
@@ -9,6 +9,20 @@ class SongContainer extends React.Component {
     };
   }
 
+  componentDidMount(){
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.addEventListener('load', () => {
+      if (request.status === 200) {
+        const jsonString = request.responseText;
+        const data = JSON.parse(jsonString);
+        this.setState({ topSongs: data });
+      }
+    });
+    request.send();
+  }
+
   render(){
     return(
       <SongList/>
